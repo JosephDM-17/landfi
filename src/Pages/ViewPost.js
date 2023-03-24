f81beaca3f515ca465f918c0a93ee83cb7b7c472
JosephDM-17 <josephdominicmathew@gmail.com>
2023-03-24 09:49:06 +0530

    Added viewPage

---
+++ src/Pages/ViewPost.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from '../Components/Navbar/Navbar'
+import View from '../Components/View/view'
+
+function ViewPost() {
+  return (
+    <div>
+        <Navigation/>
+        <View/>
+    </div>
+  )
+}
+
+export default ViewPost
